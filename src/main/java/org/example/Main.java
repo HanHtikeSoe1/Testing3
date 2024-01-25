11df11be35498d24dea772ae0a7666e13ca4752c
a5ea5em5int <ayeayemyint1073@gmail.com>
2024-01-25 11:16:27 +0630

    something changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Main {
+    public void add(int a, int b)
+    {
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
